ddd6531d81f5010b0f08ecd54390c06d75c8eb88
Florian Chen <florian.chen@tuwien.ac.at>
2025-03-17 13:00:51 +0100

    update to tf2

--- semi_supervised/mnist_input.py
+++ semi_supervised/mnist_input.py
@@ -3,10 +3,11 @@ from __future__ import print_function
 import gzip
 import os
 import urllib
+import urllib.request
 
 import numpy
 
-SOURCE_URL = 'http://yann.lecun.com/exdb/mnist/'
+SOURCE_URL = 'https://raw.githubusercontent.com/fgnt/mnist/master/'
 
 
 def maybe_download(filename, work_directory):
@@ -15,7 +16,7 @@ def maybe_download(filename, work_directory):
     os.mkdir(work_directory)
   filepath = os.path.join(work_directory, filename)
   if not os.path.exists(filepath):
-    filepath, _ = urllib.urlretrieve(SOURCE_URL + filename, filepath)
+    filepath, _ = urllib.request.urlretrieve(SOURCE_URL + filename, filepath)
     statinfo = os.stat(filepath)
     print('Succesfully downloaded', filename, statinfo.st_size, 'bytes.')
   return filepath
